7aaac1be6ca64796315969b807387b6d46309174
swikar1 <123534169+swikar1@users.noreply.github.com>
2023-01-27 11:31:40 +0530

    Update ShowingTest.java
    - More Test cases added

--- src/test/java/com/jpmc/theater/ShowingTest.java
+++ src/test/java/com/jpmc/theater/ShowingTest.java
@@ -23,6 +23,18 @@ class ShowingTest {
     }
 
     @Test
+    void testConstructor2() {
+        Movie movie = new Movie("Dr", null, 10.0, 1);
+
+        LocalDateTime ofResult = LocalDateTime.of(1, 1, 1, 1, 1);
+        Showing actualShowing = new Showing(movie, 1, ofResult);
+
+        assertSame(movie, actualShowing.getMovie());
+        assertEquals(1, actualShowing.getSequenceOfTheDay());
+        assertSame(ofResult, actualShowing.getStartTime());
+    }
+
+    @Test
     void testIsSequence() {
         Movie movie = new Movie("Dr", null, 10.0, 1);
 
@@ -37,7 +49,28 @@ class ShowingTest {
     }
 
     @Test
+    void testIsSequence3() {
+        Movie movie = new Movie("Dr", null, 10.0, 1);
+
+        assertTrue((new Showing(movie, 1, LocalDateTime.of(1, 1, 1, 1, 1))).isSequence(1));
+    }
+
+    @Test
+    void testIsSequence4() {
+        Movie movie = new Movie("Dr", null, 10.0, 1);
+
+        assertFalse((new Showing(movie, 0, LocalDateTime.of(1, 1, 1, 1, 1))).isSequence(1));
+    }
+
+    @Test
     void testGetMovieFee() {
+        Movie movie = new Movie("Dr", null, 10.0, 1);
+
+        assertEquals(10.0, (new Showing(movie, 1, LocalDateTime.of(1, 1, 1, 1, 1))).getMovieFee());
+    }
+
+    @Test
+    void testGetMovieFee2() {
         Movie movie = new Movie("Dr", null, 10.0, 1);
 
         assertEquals(10.0, (new Showing(movie, 1, LocalDateTime.of(1, 1, 1, 1, 1))).getMovieFee());
