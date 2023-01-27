573c1c1c63461b303831d13f9f03723615a32c73
swikar1 <123534169+swikar1@users.noreply.github.com>
2023-01-27 11:29:50 +0530

    Update ReservationTest.java
    - More Test case added

--- src/test/java/com/jpmc/theater/ReservationTest.java
+++ src/test/java/com/jpmc/theater/ReservationTest.java
@@ -19,7 +19,28 @@ class ReservationTest {
     }
 
     @Test
+    void testConstructor2() {
+
+        Customer customer = new Customer("Name", "42");
+
+        Movie movie = new Movie("Dr", null, 10.0, 1);
+
+        new Reservation(customer, new Showing(movie, 1, LocalDateTime.of(1, 1, 1, 1, 1)), 3);
+
+    }
+
+    @Test
     void testTotalFee() {
+        Customer customer = new Customer("Name", "42");
+
+        Movie movie = new Movie("Dr", null, 10.0, 1);
+
+        assertEquals(30.0,
+                (new Reservation(customer, new Showing(movie, 1, LocalDateTime.of(1, 1, 1, 1, 1)), 3)).totalFee());
+    }
+
+    @Test
+    void testTotalFee2() {
         Customer customer = new Customer("Name", "42");
 
         Movie movie = new Movie("Dr", null, 10.0, 1);
